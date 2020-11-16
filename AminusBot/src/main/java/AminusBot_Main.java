8576650ee3181a83f25743e13def996003194954
배수민 <62862349+Pewbe@users.noreply.github.com>
2020-11-16 13:26:22 +0900

    Add files via upload

--- AminusBot/src/main/java/AminusBot_Main.java
+++ AminusBot/src/main/java/AminusBot_Main.java
@@ -1,29 +1,58 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
 
+import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AminusBot_Main {
     public static void main( String[] args ){
-        String token = "Nzc1MjA4MzE3NDgzMDg5OTYw.X6i_AQ.zhwNeSsxHof6l5PbMe9VE4BaWpM";
+        String token = "Nzc1MjA4MzE3NDgzMDg5OTYw.X6i_AQ.rHMcprOwARPCOML9tWolfurJj3o";
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.updateActivity("\"-도움말\"이라고 해보는거야");
+
+        api.addServerJoinListener( ev ->{
+            Server server = ev.getServer();
+            printLOG( server.getName() + " 에 초대된거야" );
+        } );
 
         api.addMessageCreateListener( ev -> {
             TextChannel ch = ev.getChannel();
+            Message message = ev.getMessage();
             String msg = ev.getMessageContent();
+            EmbedBuilder embed = new EmbedBuilder();
+            String replaced, splitd, buff;//임시 스트링 변수들
+
+            embed.setColor( Color.CYAN );
 
             if( !msg.startsWith("-") ) return;
 
-            if( msg.contains("핑") ){
-                ch.sendMessage("퐁!");
+            if( msg.contains("핑") )
+                ch.sendMessage("안알랴줌");
+            else if( msg.contains("굴러") )
+                ch.sendMessage("데구르르 데굴데굴데굴");
+            else if( msg.contains("도움말") ){
+                embed.setTitle("에이마이너스 도움말")
+                        .setDescription("에이마이너스는 미니게임과 각종 기능을 이용할수 있는 봇입니다.")
+                        .addField("-[커맨드]", "아래의 명령어들은 \"-\"를 앞에 붙혀서 불러야 잘 알아들을수 있습니다.")
+                        .addField("도움말", "지금 보고 있는 도움말을 보여줍니다.")
+                        .addField("굴러", "데굴데굴 구릅니다.");
+                ch.sendMessage( embed );
+            } else if( msg.contains("삭제") ) {
+                int amount = Integer.parseInt(msg.replace("-삭제 ", ""));
+            } else if( msg.contains("에이") )
+                ch.sendMessage("나보다 똑똑하긴 하지만 구식인걸");
+            else if( msg.contains("테스트") ){
+                DBManager db = new DBManager();
+                ch.sendMessage("제대로 출력되었는지 콘솔창을 확인해줘.");
             }
-            else if( msg.contains("굴러") ){
-                ch.sendMessage("데굴데굴데굴......");
-            } else {
-                ch.sendMessage("그런거 모르는걸");
-            }
+            else
+                ch.sendMessage("그런 명령어는 아무리 뒤져봐도 없는 것 같은데..");
         } );
         printLOG("초대링크: " + api.createBotInvite());
     }
---
+++ AminusBot/src/main/java/DBManager.java
@@ -0,0 +1,60 @@
+import javax.swing.plaf.nimbus.State;
+import java.awt.*;
+import java.sql.*;
+
+public class DBManager {
+    private final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //드라이버. mysql8이후 드라이버가 변경됨
+    private final String DB_URL = "jdbc:mysql://localhost/test?serverTimezone=UTC"; //접속할 DB 서버 ?serverTimezone=UTC는 mysql8이상일때 넣어줘야함
+
+    private final String USER_NAME = "root"; //DB에 접속할 사용자 이름을 상수로 정의
+    private final String PASSWORD = ""; //사용자의 비밀번호를 상수로 정의
+
+    public DBManager(){
+        Connection conn = null;
+        Statement state = null;
+
+        try{
+            Class.forName( JDBC_DRIVER );
+            conn = DriverManager.getConnection( DB_URL, USER_NAME, PASSWORD );
+            state = conn.createStatement();
+
+            String sql; //SQL 명령문을 저장할 String
+            sql = "SELECT * FROM school";//사용할 명령어를 집어넣음
+            ResultSet res = state.executeQuery(sql); //SQL 명령문을 전달하여 실행
+
+            while(res.next()){
+                String number = res.getString("_number");
+                String name = res.getString("name");
+                String position = res.getString("position");
+                String age = res.getString("age");
+
+                System.out.println("번호: " + number);
+                System.out.println("이름: " + name);
+                System.out.println("직책: " + position);
+                System.out.println("나이: " + age);
+                System.out.println("-------------------------------");
+            }
+
+            //모두 닫음
+            res.close();
+            state.close();
+            conn.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally { //예외가 있든 없든 무조건 실행. state와 conn이 만약 닫히지 않았을 때 제대로 닫아주는건듯
+            try{
+                if(state!=null)
+                    state.close();
+            }catch(SQLException ex1){
+                ex1.printStackTrace();
+            }
+
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException ex1){
+                ex1.printStackTrace();
+            }
+        }
+    }
+}
